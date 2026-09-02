e15dab2e04376542939315e7f8429ba7e93bfc03
kimjeon-il <55841736+kimjeon-il@users.noreply.github.com>
2026-09-02 14:10:32 +0900

    Expose river partition through Worker RPC

--- assets/js/workers/river-territory-partition-worker.js
+++ assets/js/workers/river-territory-partition-worker.js
@@ -1,9 +1,14 @@
 const revision = new URL(self.location.href).searchParams.get('v') || '';
 const partitionModuleUrl = new URL('../modules/river-territory-partition.js', self.location.href);
 const polygonClippingUrl = new URL('../vendor/polygon-clipping.min.js', self.location.href);
-if (revision) partitionModuleUrl.searchParams.set('v', revision);
-if (revision) polygonClippingUrl.searchParams.set('v', revision);
+const rpcHostUrl = new URL('./worker-rpc-host.js', self.location.href);
+if (revision) {
+  partitionModuleUrl.searchParams.set('v', revision);
+  polygonClippingUrl.searchParams.set('v', revision);
+  rpcHostUrl.searchParams.set('v', revision);
+}
 
+importScripts(rpcHostUrl.href);
 const dependencies = Promise.all([
   import(partitionModuleUrl.href),
   import(polygonClippingUrl.href),
@@ -12,12 +17,26 @@ const dependencies = Promise.all([
   clipper: self.polygonClipping || globalThis.polygonClipping || null,
 }));
 
-self.onmessage = async event => {
+async function compute(payload, context = null) {
+  const { partitionModule, clipper } = await dependencies;
+  context?.throwIfCancelled?.();
+  const result = partitionModule.buildRiverTerritoryPartitions({ ...(payload || {}), clipper });
+  context?.throwIfCancelled?.();
+  return result;
+}
+
+self.PandoLabWorkerRpc.install({
+  handlers: {
+    'river.partition': (payload, context) => compute(payload, context),
+  },
+});
+
+// Compatibility bridge for the current annex controller.
+self.addEventListener('message', async event => {
   const message = event.data || {};
-  if (message.type !== 'compute') return;
+  if (message.rpc === self.PandoLabWorkerRpc.PROTOCOL || message.type !== 'compute') return;
   try {
-    const { partitionModule, clipper } = await dependencies;
-    const result = partitionModule.buildRiverTerritoryPartitions({ ...(message.payload || {}), clipper });
+    const result = await compute(message.payload || {});
     self.postMessage({ type: 'result', requestId: message.requestId, result });
   } catch (error) {
     self.postMessage({
@@ -27,4 +46,4 @@ self.onmessage = async event => {
       stack: error?.stack || '',
     });
   }
-};
+});
