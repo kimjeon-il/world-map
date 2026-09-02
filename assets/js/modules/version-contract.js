5ac57ccb107226c4f19acb606fac70125e5977fd
kimjeon-il <55841736+kimjeon-il@users.noreply.github.com>
2026-09-02 13:36:57 +0900

    Define version and schema contracts

---
+++ assets/js/modules/version-contract.js
@@ -0,0 +1,40 @@
+export const PROJECT_SCHEMA_VERSION = 4;
+export const MIN_SUPPORTED_PROJECT_SCHEMA_VERSION = 3;
+
+export const LAND_OBJECT_SCHEMA_VERSION = 2;
+export const SOURCE_PROVENANCE_SCHEMA_VERSION = 1;
+export const GENERIC_FEATURE_SCHEMA_VERSION = 2;
+export const TERRITORIAL_MODEL_SCHEMA_VERSION = 1;
+export const DISTRIBUTION_MODEL_SCHEMA_VERSION = 2;
+export const LAYER_PRESENTATION_SCHEMA_VERSION = 2;
+
+export const VERSION_CHANGE_LEVELS = Object.freeze({
+  MINOR: 'minor',
+  PATCH: 'patch',
+});
+
+export const VERSION_POLICY = Object.freeze({
+  appVersionSource: 'package.json',
+  minor: Object.freeze([
+    'user-visible feature',
+    'UX system change',
+    'architecture contract change',
+    'save-format capability change',
+  ]),
+  patch: Object.freeze([
+    'bug fix',
+    'visual polish',
+    'performance improvement without contract change',
+    'internal refactor without contract change',
+  ]),
+  projectSchemaIndependentFromAppVersion: true,
+  datasetVersionsIndependentFromAppVersion: true,
+  assetRevisionSource: 'build metadata',
+});
+
+export function isSupportedProjectSchemaVersion(value) {
+  const version = Number(value);
+  return Number.isInteger(version)
+    && version >= MIN_SUPPORTED_PROJECT_SCHEMA_VERSION
+    && version <= PROJECT_SCHEMA_VERSION;
+}
